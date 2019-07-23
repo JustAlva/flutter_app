8f7bf459663cb70bff2725cd607ba7d9591d9d9b
keding.zhou <zkdjust@gmail.com>
2019-07-23 14:20:09 +0800

    Initial commit

---
+++ lib/splash.dart
@@ -0,0 +1,15 @@
+// 控件等导入.
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyWidget());
+}
+
+class MyWidget extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+}
